d4917bcb9e6a952596bdcaed2ac7c0a4ad3206fb
Ejibi <144816857+Ejibi@users.noreply.github.com>
2024-04-26 23:17:09 -0600

    Add files via upload

---
+++ Simple Bash scripting and obtaining the divisors of any number/divisors.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int x;
+    int i=0;
+    scanf("%d",&x);
+    while(x<=i,++i){
+        if (x%i==0 && i!=1){
+        printf(" %d",i);}
+        if(x == i){
+            printf("\n");
+            break;
+        }
+        if (i == 1){
+        printf("%d",1);
+        }
+    }
+    }
+
